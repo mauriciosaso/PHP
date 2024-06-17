546b6bb25cc4455ec0aea9891ee3d9d8edac6694
03457072060 <03457072060@senacrs.edu.br>
2024-06-17 20:10:16 -0300

    Aula2

---
+++ 2aula/index.php
@@ -0,0 +1,11 @@
+<?php
+$nome = "Mauricio";
+$idade = 21;
+$altura = 1.75;
+$logico = true;
+
+    echo "aula 1" . "<br>"; 
+    echo " <br> ";
+
+    echo "Nome:" . $nome . "  <br> ". "Idade:" . $idade . "  <br>  " . "Altura:" . " " . $altura . "  <br>  " . "Logico:" . " " . $logico; 
+?>
