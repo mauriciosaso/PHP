e5990dccc820b90869aa08b0488f6593373a787b
03457072060 <03457072060@senacrs.edu.br>
2024-06-17 20:14:59 -0300

    Aula2 v2

--- 2aula/index.php
+++ 2aula/index.php
@@ -3,9 +3,10 @@ $nome = "Mauricio";
 $idade = 21;
 $altura = 1.75;
 $logico = true;
+const senai = "Caxias";
 
     echo "aula 1" . "<br>"; 
     echo " <br> ";
 
-    echo "Nome:" . $nome . "  <br> ". "Idade:" . $idade . "  <br>  " . "Altura:" . " " . $altura . "  <br>  " . "Logico:" . " " . $logico; 
+    echo "Nome:" . $nome . "  <br> ". "Idade:" . $idade . "  <br>  " . "Altura:" . " " . $altura . "  <br>  " . "Logico:" . " " . $logico . "  <br>  " . "Local:" . " " . senai; 
 ?>
