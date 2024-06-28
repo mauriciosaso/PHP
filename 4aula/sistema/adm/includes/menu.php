3afede9a120c8ceb1a34584a834bdecf97887a29
03457072060 <03457072060@senacrs.edu.br>
2024-06-28 19:24:52 -0300

    Aula 4

--- 4aula/sistema/adm/includes/menu.php
+++ 4aula/sistema/adm/includes/menu.php
@@ -23,6 +23,7 @@
           <a class="dropdown-item" href="cadastroEstado.php">Estado</a>
           <a class="dropdown-item" href="cadastroCidade.php">Cidade</a>
           <a class="dropdown-item" href="envioEmail.php">Envio de Email</a>
+          <a class="dropdown-item" href="avaliacao.php">Avaliação</a>
         </div>
       </li>
   <li class="nav-item">
